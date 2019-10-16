95eded04d24c95059562398ed016be6374dd2e65
S1dddyyy <56650861+S1dddyyy@users.noreply.github.com>
2019-10-16 23:33:06 +0530

    easy way to reverse array

--- reverse_array.c
+++ reverse_array.c
@@ -1,51 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-#define MAX_BUFFER_SIZE 100
-#define TRUE 1
-#define FALSE !TRUE
-
-int reverse_array(int size, char * buffer, char * output)
+ 
+int main()
 {
-    int count = 0;
-
-    if(size > 0)
-    {
-    for (count = 0; count < size; count++)
-        {
-            output[count] = buffer[size - count - 1];
-        }
-        output[size] = '\0';
-        return TRUE;
-    }
-    else
-    {
-        return FALSE;
-    }
-}
-
-void main()
-{
-    char input_buffer[MAX_BUFFER_SIZE], output_buffer[MAX_BUFFER_SIZE];
-    int count = 0, buffer_length;
-
-    printf("Enter a string of characters to reverse.\n");
-    printf("For example: 12345. Up to 100 characters.\n");
-    fflush(stdout);
-
-    if(fgets(input_buffer, MAX_BUFFER_SIZE, stdin) == NULL)
-    {
-        printf("Noting entered, BYE!\n");
-        return;
-    }
-
-    buffer_length = strlen(input_buffer) - 1;
-    if (input_buffer[buffer_length] == '\n')
-        input_buffer[buffer_length] = '\0';
-    printf("You entered:\n%s\n", input_buffer);
-    fflush(stdout);
-
-    if (reverse_array(buffer_length, input_buffer, output_buffer) == FALSE)
-        printf("Error reversing array\n");
-    else
-        printf("Your string has been reversed:\n%s\nBYE!\n", output_buffer);
+   int n, c, d, a[100], b[100];
+ 
+   printf("Enter the number of elements in array\n");
+   scanf("%d", &n);
+ 
+   printf("Enter array elements\n");
+ 
+   for (c = 0; c < n ; c++)
+      scanf("%d", &a[c]);
+ 
+   /*
+    * Copying elements into array b starting from end of array a
+    */
+ 
+   for (c = n - 1, d = 0; c >= 0; c--, d++)
+      b[d] = a[c];
+ 
+   /*
+    * Copying reversed array into the original.
+    * Here we are modifying original array, this is optional.
+    */
+ 
+   for (c = 0; c < n; c++)
+      a[c] = b[c];
+ 
+   printf("Reverse array is\n");
+ 
+   for (c = 0; c < n; c++)
+      printf("%d\n", a[c]);
+ 
+   return 0;
 }
